fdbd92fbd699f2c6847b0da51262a8dcf2d21858
Kei Wen Huang <kaoghk@gmail.com>
2018-12-07 16:32:13 -0800

    added listitem layout

--- app/src/main/java/com/example/android/tastipe/Fragment/DraftIngredientFragment.java
+++ app/src/main/java/com/example/android/tastipe/Fragment/DraftIngredientFragment.java
@@ -3,20 +3,83 @@ package com.example.android.tastipe.Fragment;
  * Created by kevin on 11/10/18.
  */
 
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.design.widget.Snackbar;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.helper.ItemTouchHelper;
+import android.view.View;
+import android.widget.FrameLayout;
 
+import com.example.android.tastipe.Adapter.ListItemAdapter;
+import com.example.android.tastipe.Model.Ingredients;
 import com.example.android.tastipe.Model.Recipe;
 import com.example.android.tastipe.R;
+import com.example.android.tastipe.Utils.RecyclerItemTouchHelper;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * TODO: Add a class header comment!
  */
-public class DraftIngredientFragment extends DefaultFragment {
+public class DraftIngredientFragment extends DefaultFragment implements RecyclerItemTouchHelper.RecyclerItemTouchHelperListener {
 
     private Recipe mRecipe;
 
     private RecyclerView mRecyclerView;
+    private List<Ingredients> mIngredientsList = new ArrayList<>();
+    private ListItemAdapter mAdapter;
+    private FrameLayout mFrameLayout;
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+
+        mIngredientsList.add(new Ingredients("Apple", "2"));
+        mIngredientsList.add(new Ingredients("Onion", "0.5"));
+        mIngredientsList.add(new Ingredients("Bay Leaf", "5"));
+        mIngredientsList.add(new Ingredients("Beef", "1"));
+
+
+        mFrameLayout = view.findViewById(R.id.list_item_container);
+
+        mRecyclerView = view.findViewById(R.id.recyclerView);
+        mRecyclerView.setHasFixedSize(true);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        mAdapter = new ListItemAdapter(mIngredientsList);
+        mRecyclerView.setAdapter(mAdapter);
+
+        ItemTouchHelper.SimpleCallback itemTouchHelperCallback = new RecyclerItemTouchHelper(0, ItemTouchHelper.LEFT, this);
+        new ItemTouchHelper(itemTouchHelperCallback).attachToRecyclerView(mRecyclerView);
+    }
+
+    @Override
+    public void onSwiped(RecyclerView.ViewHolder viewHolder, int direction, int position) {
+        if (viewHolder instanceof ListItemAdapter.ListItemViewHolder) {
+            String name = mIngredientsList.get(viewHolder.getAdapterPosition()).getName();
+
+            final Ingredients deletedItem = mIngredientsList.get(viewHolder.getAdapterPosition());
+            final int deletedIndex = viewHolder.getAdapterPosition();
+
+            mAdapter.removeItem(viewHolder.getAdapterPosition());
+
+            Snackbar snackbar = Snackbar.make(mFrameLayout, name + " remove from list!", Snackbar.LENGTH_LONG);
+            snackbar.setAction("UNDO", new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    mAdapter.restoreItem(deletedItem, deletedIndex);
+                }
+            });
+            snackbar.setActionTextColor(Color.WHITE);
+            snackbar.show();
+        }
+    }
     public void putArguments(Recipe recipe) {
         mRecipe = recipe;
     }
@@ -25,4 +88,6 @@ public class DraftIngredientFragment extends DefaultFragment {
     protected int layoutRes() {
         return R.layout.fragment_draft_ingredient;
     }
+
+
 }
--- app/src/main/java/com/example/android/tastipe/Model/Ingredients.java
+++ app/src/main/java/com/example/android/tastipe/Model/Ingredients.java
@@ -15,11 +15,29 @@ public class Ingredients implements Serializable {
     @SerializedName("name")
     private String name;
 
+    private String quantity;
+
+    public Ingredients() {
+    }
+
+    public Ingredients(String name, String quantity) {
+        this.name = name;
+        this.quantity = quantity;
+    }
+
     public String getName() {
         return name;
     }
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(String quantity) {
+        this.quantity = quantity;
     }
 }
